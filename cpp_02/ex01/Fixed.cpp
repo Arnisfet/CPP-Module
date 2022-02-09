aaab1be6d84399eaf6fb35846d6b8c2d1bf634be
Колиух Виктор Александрович <VKoliukh@cbgr.ru>
2022-02-09 11:13:26 +0300

    ex_02

---
+++ cpp_02/ex01/Fixed.cpp
@@ -0,0 +1,61 @@
+//
+// Created by VKoliukh on 09.02.2022.
+//
+
+#include "Fixed.h"
+
+
+
+#define FIXED_POINT_ONE (1 << 8)
+
+
+Fixed::Fixed() : fixedPointValue(0) {
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int x) {
+    std::cout << "Int constructor called" << std::endl;
+    fixedPointValue = (x << NUM_FRAC_BITS);
+}
+
+Fixed::Fixed(const float f)  {
+    std::cout << "Float constructor called" << std::endl;
+    fixedPointValue = std::roundf(f * FIXED_POINT_ONE);
+}
+
+Fixed::~Fixed() {
+    std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits() const {
+    return fixedPointValue;
+}
+
+void Fixed::setRawBits(int const raw) {
+    fixedPointValue = raw;
+}
+
+Fixed::Fixed(Fixed const &other) {
+    std::cout << "Copy constructor called" << std::endl;
+    *this = other;
+}
+
+Fixed &Fixed::operator=(const Fixed &other) {
+    std::cout << "Assignation operator called" << std::endl;
+    if (this != &other)
+        this->fixedPointValue = other.getRawBits();
+    return *this;
+}
+
+float Fixed::toFloat() const {
+    return (float )fixedPointValue / FIXED_POINT_ONE;
+}
+
+int Fixed::toInt() const {
+    return fixedPointValue >> NUM_FRAC_BITS;
+}
+
+std::ostream &operator<<(std::ostream &ostream, Fixed const &fixed) {
+    ostream << fixed.toFloat();
+    return ostream;
+}
---
+++ cpp_02/ex01/Fixed.h
@@ -0,0 +1,34 @@
+//
+// Created by VKoliukh on 09.02.2022.
+//
+
+#ifndef EX01_FIXED_H
+#define EX01_FIXED_H
+
+#include <iostream>
+#include <cmath>
+
+class Fixed {
+public:
+    Fixed();
+    Fixed(const int x);
+    Fixed(const Fixed &other);
+    Fixed(float const f);
+    ~Fixed();
+
+    Fixed & operator=(const Fixed &other);
+
+    int getRawBits() const;
+    void setRawBits(int raw);
+
+    float toFloat() const;
+    int toInt() const;
+
+private:
+    int fixedPointValue;
+    static const int NUM_FRAC_BITS = 8;
+};
+
+std::ostream &operator<<(std::ostream &ostream, Fixed const &fixed);
+
+#endif //EX01_FIXED_H
