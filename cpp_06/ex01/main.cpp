a7cf562928f21066d1966216f489b2cada52574e
Arnisfet <mrudge@student.21-school.ru>
2022-03-02 00:04:29 +0300

    Make ex01 in cpp06

--- cpp_06/ex01/main.cpp
+++ cpp_06/ex01/main.cpp
@@ -1,51 +1,27 @@
-#include "Bureaucrat.h"
-#include "Form.h"
+#include <iostream>
+#include <stdint.h>
 
-int main()
-{
-	try
-	{
-		Form fo1(false, "first", 10, 30);
-		std::cout << fo1 <<std::endl;
-	}
-	catch(std::exception const &e)
-	{
-		std::cout<<e.what()<<std::endl;
-	}
-	try
-	{
-		Form fo2(false, "second", 0, 30);
-	}
-	catch(std::exception const &e)
-	{
-		std::cout<<e.what()<<std::endl;
-	}
-	try
-	{
-		Form fo3(false, "third", 10, 200);
-	}
-	catch(std::exception const &e)
-	{
-		std::cout<<e.what()<<std::endl;
-	}
-	Bureaucrat bu1("bu1", 11);
-	Bureaucrat bu2("bu2", 9);
-	Form fo4(false, "fo4", 10, 30);
-	try
-	{
-		fo4.beSigned(bu2);
-	}
-	catch(std::exception const &e)
-	{
-		std::cout<<e.what()<<std::endl;
-	}
-	try
-	{
-		fo4.beSigned(bu1);
-	}
-	catch(std::exception const &e)
-	{
-		std::cout<<e.what()<<std::endl;
-	}
+typedef struct  s_data {
+	int         num;
+	std::string str;
+}               Data;
+
+uintptr_t   serialize(Data* ptr) {
+	return (reinterpret_cast<uintptr_t>(ptr));
+}
+
+Data*       deserialized(uintptr_t raw) {
+	return (reinterpret_cast<Data*>(raw));
+}
+
+int     main() {
+	Data data;
+	Data *result;
+
+	data.num = 2142;
+	data.str = "Hello World!\n";
+	result = deserialized(serialize(&data));
+	std::cout << result->str << result->num << std::endl;
+
 	return (0);
 }
